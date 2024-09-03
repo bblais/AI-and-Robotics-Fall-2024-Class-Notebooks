cee37838aec69c0c7c189c80801b6be4225d7564
Brian Blais <bblais@gmail.com>
2024-09-03 14:22:46 -0400

    first day

---
+++ Sprint #1 - Programming and Games/2024-09-03 - First AI Class.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("hello world")
+
+
+# In[ ]:
+
+
+634638237*734673463-674654654
+
+
+# In[ ]:
+
+
+cos(90)
+
+
+# In[ ]:
+
+
+from numpy import cos
+
+
+# In[ ]:
+
+
+cos(90)
+
+
+# In[ ]:
+
+
+cos(90)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+coss(90)
+
+
+# In[ ]:
+
+
+from numpy import radians
+
+
+# In[ ]:
+
+
+cos(radians(90))
+
+
+# In[ ]:
+
+
+a=5
+b=10
+c=a*b + b**2
+
+
+# In[ ]:
+
+
+c
+
+
+# Text like this isn't python code.  It's notes.
+# 
+# **bold** *italic*
+# 
+# # heading 1
+# 
+# some text
+# 
+# ## heading 2
+# 
+# ### heading 3
+# 
+# https://www.markdownguide.org/basic-syntax/
+
+# In[ ]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+
+
