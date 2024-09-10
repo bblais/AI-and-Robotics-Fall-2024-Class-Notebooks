837532d664a7480423b17a7ff141f90c5bd9be18
Brian Blais <bblais@gmail.com>
2024-09-10 11:33:33 -0400

    instal

---
+++ Install and Update Packages.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Sprint #1
+
+# In[1]:
+
+
+get_ipython().system('pip install "git+https://github.com/bblais/Game" --upgrade')
+
+
+# In[2]:
+
+
+import Game
+
+
+# ## Sprint #2
+
+# Run this on the robot to update the software
+
+# In[ ]:
+
+
+pip install "git+https://github.com/bblais/Robot373" --upgrade
+
+
+# ## Sprint #3
+
+# (Windows only) Install Putty for pscp:  https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html
+
+# ## Sprint #4
+
+# In[3]:
+
+
+get_ipython().system('pip install scikit-learn')
+
+
+# In[4]:
+
+
+get_ipython().system('pip install "git+https://github.com/bblais/classy" --upgrade')
+
+
+# In[1]:
+
+
+get_ipython().system('pip install "git+https://github.com/bblais/NumpyNet" --upgrade')
+
+
+# In[3]:
+
+
+get_ipython().system('pip install tqdm')
+
+
+# In[1]:
+
+
+get_ipython().system('pip install opencv-python')
+
+
+# In[ ]:
+
+
+
+
