8a85a899b2c70eb1a59ea9c56aee007ebada0e0f
Brian Blais <bblais@gmail.com>
2024-09-17 12:49:07 -0400

    misc

---
+++ Sprint #1 - Programming and Games/2024-09-17 - Misc.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("bob")
+print("sally")
+
+print("bob")
+print("sally")
+
+print("bob")
+print("sally")
+
+print("bob")
+print("sally")
+
+print("bob")
+print("sally")
+
+print("bob")
+print("sally")
+
+print("bob")
+print("sally")
+
+
+# In[ ]:
+
+
+for i in range(10):
+    print("bob")
+    print("sally")
+    
+
+
+# In[ ]:
+
+
+for i in [10,20,30,40]:
+    print(i)
+
+
+# In[ ]:
+
+
+list(range(10))
+
+
+# In[ ]:
+
+
+reset()
+for i in range(10):
+    print(i)
+    
+#draw some more
+
+
+# In[ ]:
+
+
+
+
