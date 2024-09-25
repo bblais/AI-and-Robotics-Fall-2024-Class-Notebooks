88e3e59e1dc65f0285d4ca5465fcdd864b24c1ea
Brian Blais <bblais@gmail.com>
2024-09-25 06:00:04 -0400

    skittles

--- Sprint #2 - Learning and Robots /2024-09-24 - TTT Skittles v Skittles-Copy1.py
+++ Sprint #2 - Learning and Robots /2024-09-24 - TTT Skittles v Skittles-Copy1.py
@@ -8,53 +8,125 @@ from Game import *
 from Game.minimax import *
 
 
-# ## Nim 
+# ## Tic Tac Toe
 
 # In[ ]:
 
 
-def initial_state(): 
-    """ returns  - The initial state of the game"""
-    return 21
+def initial_state():
+    return Board(3,3)
+
+def count_pieces(state):
+    count=0
+    for location in range(9):
+        if state[location]!=0:
+            count=count+1
+            
+    return count
+
+def row(state,location):
+    r,c=state.rc_from_index(location)
+    return r
+def col(state,location):
+    r,c=state.rc_from_index(location)
+    return c
 
 def valid_moves(state,player):
-    """returns  - a list of the valid moves for the state and player"""
-
-    if state==2:
-        return [1,2]
-    elif state==1:
-        return [1]
+    
+    moves=[]
+    
+    if count_pieces(state)<9:  # placement - temporaily set to 9 to behave like ttt
+        # placement
+        for location in range(9): 
+            if state[location]==0:
+                moves.append(location)
     else:
-        return [1,2,3]
+        # sliding
+        for start in range(9):
+            if state[start]!=player:
+                continue
 
-def show_state(state,player):
-    """prints or shows the current state"""
-    print(f"There are >>{state} sticks<<< for player {player}.")
+            rs,cs=row(state,start),col(state,start)
 
+            for location in range(9):
+                end=None
+
+                r,c=row(state,location),col(state,location)
+
+                if state[location]==0:
+
+                    if r-rs==1 and c-cs==0:  # vertical
+                        end=location
+                    if r-rs==-1 and c-cs==0:  # vertical
+                        end=location
+                    if r-rs==0 and c-cs==1:  # horizontal
+                        end=location
+                    if r-rs==0 and c-cs==-1:  # horizontal
+                        end=location
+                    if r-rs==1 and c-cs==1:  # diagonal
+                        end=location
+                    if r-rs==-1 and c-cs==-1:  # diagonal
+                        end=location
+                    if r-rs==1 and c-cs==-1:  # diagonal
+                        end=location
+                    if r-rs==-1 and c-cs==1:  # diagonal
+                        end=location
+
+                    if not end is None:
+                        moves.append([start,end])
+                pass
+
+        
+    return moves
+    
+    
 def update_state(state,player,move):
-    """returns  - the new state after the move for the player"""
-
-    new_state=state-move
-
+    if isinstance(move,int):  # placement
+        new_state=state
+        new_state[move]=player
+        
+    else:  # sliding
+        start,end=move
+        new_state=state
+        new_state[start]=0
+        new_state[end]=player
+        
     return new_state
 
-
 def win_status(state,player):
-    """    returns  - 'win'  if the state is a winning state for the player, 
-               'lose' if the state is a losing state for the player,
-               'stalemate' for a stalemate
-               None otherwise
-    """
+    # 0  1  2 
+    # 3  4  5 
+    # 6  7  8 
 
-    if state==0:
-        return 'lose'
-
-    if state==1:
-        return 'win'
-
-
-    return None
+    if player==1:
+        other_player=2
+    else:
+        other_player=1
     
+    if state[0]==state[1]==state[2]==player:
+        return "win"
+    if state[3]==state[4]==state[5]==player:
+        return "win"
+    if state[6]==state[7]==state[8]==player:
+        return "win"
+    if state[0]==state[3]==state[6]==player:
+        return "win"
+    if state[1]==state[4]==state[7]==player:
+        return "win"
+    if state[2]==state[5]==state[8]==player:
+        return "win"
+    if state[0]==state[4]==state[8]==player:
+        return "win"
+    if state[6]==state[4]==state[2]==player:
+        return "win"
+    
+    if not valid_moves(state,other_player):
+        return "stalemate"
+    
+    return None
+
+def show_state(state,player):
+    print(state)
 
 
 # ## Agents
@@ -203,32 +275,12 @@ def skittles_after(status,player,info):
 skittles_agent1=Agent(skittles_move)
 skittles_agent1.S=Table()
 skittles_agent1.post=skittles_after
-skittles_agent1.verbose=False
+skittles_agent1.verbose=True
 
 skittles_agent2=Agent(skittles_move)
 skittles_agent2.S=Table()
 skittles_agent2.post=skittles_after
-skittles_agent2.verbose=False
-
-
-# In[ ]:
-
-
-def perfect_move(state,player):
-    move=(state-1)%4
-
-    if move==0:  # bad state
-        move=1
-
-    return move
-
-perfect_agent=Agent(perfect_move)
-
-
-# In[ ]:
-
-
-
+skittles_agent2.verbose=True
 
 
 # In[ ]:
@@ -241,8 +293,22 @@ g.run(skittles_agent1,skittles_agent2)
 # In[ ]:
 
 
-SaveTable(skittles_agent1.S,'nim skittles1.json')
-SaveTable(skittles_agent2.S,'nim skittles2.json')
+SaveTable(skittles_agent1.S,'ttt skittles1.json')
+SaveTable(skittles_agent2.S,'ttt skittles2.json')
+
+
+# In[ ]:
+
+
+skittles_agent1=Agent(skittles_move)
+skittles_agent1.S=Table()
+skittles_agent1.post=skittles_after
+skittles_agent1.verbose=False
+
+skittles_agent2=Agent(skittles_move)
+skittles_agent2.S=Table()
+skittles_agent2.post=skittles_after
+skittles_agent2.verbose=False
 
 
 # In[ ]:
@@ -300,17 +366,32 @@ ylabel('Percent')
 # In[ ]:
 
 
-SaveTable(skittles_agent1.S,'nim skittles1.json')
-SaveTable(skittles_agent2.S,'nim skittles2.json')
+for i in tqdm(range(1000)):
+    g=Game(number_of_games=100)
+    g.display=False
+    
+    result=g.run(agent1,agent2)
+    one,two,ties,N=one+result.count(1),two+result.count(2),ties+result.count(0),N+len(result)
+    
+    S+=one/N*100,two/N*100,ties/N*100,N
+    
+y1,y2,y0,x=S.arrays()   
 
 
-# ## Test the agent
+figure(figsize=(16,8))
+plot(x,y1,label='One Win')
+plot(x,y2,label='Two Win')
+plot(x,y0,label='Tie')
+legend()
+xlabel('Number of Games')
+ylabel('Percent')
+
 
 # In[ ]:
 
 
-g=Game()
-g.run(human_agent,skittles_agent2)
+SaveTable(skittles_agent1.S,'ttt skittles1.json')
+SaveTable(skittles_agent2.S,'ttt skittles2.json')
 
 
 # In[ ]:
@@ -318,12 +399,15 @@ g.run(human_agent,skittles_agent2)
 
 g=Game(number_of_games=100)
 g.display=False
-result=g.run(minimax_agent,agent2)
+result=g.run(minimax_agent,skittles_agent2)
 g.report()
 
 
 # In[ ]:
 
 
-
+g=Game(number_of_games=100)
+g.display=False
+result=g.run(skittles_agent1,minimax_agent)
+g.report()
 
