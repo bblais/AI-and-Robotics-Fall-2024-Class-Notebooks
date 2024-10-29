0e7871b304d2c9f6e75511f164b1e41aafe9a84c
Brian Blais <bblais@gmail.com>
2024-10-29 15:18:56 -0400

    stochastic and robots

---
+++ Sprint #3 - Probability and Robots/2024-10-15 - TTT Q v Q - Loading the Table.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from Game import *
+from Game.minimax import *
+from tqdm import tqdm
+
+
+# ## TTT
+
+# In[2]:
+
+
+def initial_state():
+    return Board(3,3)
+
+def count_pieces(state):
+    count=0
+    for location in range(9):
+        if state[location]!=0:
+            count=count+1
+            
+    return count
+
+def row(state,location):
+    r,c=state.rc_from_index(location)
+    return r
+def col(state,location):
+    r,c=state.rc_from_index(location)
+    return c
+
+def valid_moves(state,player):
+    
+    moves=[]
+    
+    if count_pieces(state)<9:  # placement - temporaily set to 9 to behave like ttt
+        # placement
+        for location in range(9): 
+            if state[location]==0:
+                moves.append(location)
+    else:
+        # sliding
+        for start in range(9):
+            if state[start]!=player:
+                continue
+
+            rs,cs=row(state,start),col(state,start)
+
+            for location in range(9):
+                end=None
+
+                r,c=row(state,location),col(state,location)
+
+                if state[location]==0:
+
+                    if r-rs==1 and c-cs==0:  # vertical
+                        end=location
+                    if r-rs==-1 and c-cs==0:  # vertical
+                        end=location
+                    if r-rs==0 and c-cs==1:  # horizontal
+                        end=location
+                    if r-rs==0 and c-cs==-1:  # horizontal
+                        end=location
+                    if r-rs==1 and c-cs==1:  # diagonal
+                        end=location
+                    if r-rs==-1 and c-cs==-1:  # diagonal
+                        end=location
+                    if r-rs==1 and c-cs==-1:  # diagonal
+                        end=location
+                    if r-rs==-1 and c-cs==1:  # diagonal
+                        end=location
+
+                    if not end is None:
+                        moves.append([start,end])
+                pass
+
+        
+    return moves
+    
+    
+def update_state(state,player,move):
+    if isinstance(move,int):  # placement
+        new_state=state
+        new_state[move]=player
+        
+    else:  # sliding
+        start,end=move
+        new_state=state
+        new_state[start]=0
+        new_state[end]=player
+        
+    return new_state
+
+def win_status(state,player):
+    # 0  1  2 
+    # 3  4  5 
+    # 6  7  8 
+
+    if player==1:
+        other_player=2
+    else:
+        other_player=1
+    
+    if state[0]==state[1]==state[2]==player:
+        return "win"
+    if state[3]==state[4]==state[5]==player:
+        return "win"
+    if state[6]==state[7]==state[8]==player:
+        return "win"
+    if state[0]==state[3]==state[6]==player:
+        return "win"
+    if state[1]==state[4]==state[7]==player:
+        return "win"
+    if state[2]==state[5]==state[8]==player:
+        return "win"
+    if state[0]==state[4]==state[8]==player:
+        return "win"
+    if state[6]==state[4]==state[2]==player:
+        return "win"
+    
+    if not valid_moves(state,other_player):
+        return "stalemate"
+    
+    return None
+
+def show_state(state,player):
+    print(state)
+
+
+# ## Agents
+
+# In[3]:
+
+
+def random_move(state,player):    
+    moves=valid_moves(state,player)
+    return random.choice(moves)
+
+random_agent=Agent(random_move)
+
+def human_move(state,player):
+    print("Player ", player)
+    valid_move=False
+    while not valid_move:
+        move=int(input('What is your move? '))
+
+        if move in valid_moves(state,player):
+            valid_move=True
+        else:
+            print("Illegal move.")
+
+    return move
+human_agent=Agent(human_move)
+
+
+# In[8]:
+
+
+def minimax_move(state,player):
+    values,moves=minimax_values(state,player,display=False)
+    return top_choice(moves,values)
+
+
+minimax_agent=Agent(minimax_move)
+
+
+# ## Q Agent
+
+# In[4]:
+
+
+def Q_move(state,player,info):
+    Q=info.Q
+    last_state=info.last_state
+    last_action=info.last_action
+    learning=info.learning
+    
+    α=info.α  # learning rate
+    ϵ=info.ϵ  # how often to take a random move
+    γ=info.γ  # memory constant -- how quickly does the table update back in time (earlier in the game)
+    
+    # \alpha <hit tab>    α
+    # \epsilon <hit tab>  ϵ
+    # \gamma <hit tab>    γ
+    
+    if state not in Q:
+        actions=valid_moves(state,player)
+        Q[state]=Table()
+        for action in actions:
+            Q[state][action]=0  # initial value of table
+    
+    if learning:
+        if random.random()<ϵ:  # take a random move occasionally to explore the environment
+            move=random_move(state,player)
+        else:
+            move=top_choice(Q[state])
+    else:
+        move=top_choice(Q[state])
+    
+    if not last_action is None:  # not the first move
+        reward=0
+        
+        # learn
+        if learning:
+            Q[last_state][last_action]+=α*(reward +
+                        γ*max([Q[state][a] for a in Q[state]]) - Q[last_state][last_action])
+    
+    return move
+
+
+# In[5]:
+
+
+def Q_after(status,player,info):
+    Q=info.Q
+    last_state=info.last_state
+    last_action=info.last_action
+    learning=info.learning
+    
+    α=info.α  # learning rate
+    ϵ=info.ϵ  # how often to take a random move
+    γ=info.γ  # memory constant -- how quickly does the table update back in time (earlier in the game)
+    
+    # \alpha <hit tab>    α
+    # \epsilon <hit tab>  ϵ
+    # \gamma <hit tab>    γ
+
+    if status=='lose':
+        reward=-1
+    elif status=='win':
+        reward=1
+    elif status=='stalemate':
+        reward=.5 # value stalemate a little closer to a win
+    else:
+        reward=0
+    
+    
+    if learning:
+        Q[last_state][last_action]+=α*(reward - Q[last_state][last_action])
+        
+
+
+# In[6]:
+
+
+Q1_agent=Agent(Q_move)
+Q1_agent.post=Q_after
+Q1_agent.Q=Table()  # makes an empty table
+Q1_agent.learning=True
+
+Q1_agent.α=0.4  # learning rate
+Q1_agent.ϵ=0.5  # how often to take a random move
+Q1_agent.γ=0.9  # memory constant -- how quickly does the table update back in time (earlier in the game)
+
+
+# In[7]:
+
+
+Q2_agent=Agent(Q_move)
+Q2_agent.post=Q_after
+Q2_agent.Q=Table()  # makes an empty table
+Q2_agent.learning=True
+
+Q2_agent.α=0.4  # learning rate
+Q2_agent.ϵ=0.5  # how often to take a random move
+Q2_agent.γ=0.9  # memory constant -- how quickly does the table update back in time (earlier in the game)
+
+
+# ## Load the previous table
+
+# In[15]:
+
+
+Q1_agent.Q=LoadTable('2024-10-15 TTT Q1 Table.json')
+Q2_agent.Q=LoadTable('2024-10-15 TTT Q2 Table.json')
+
+
+# ## Test
+
+# In[16]:
+
+
+Q1_agent.learning=False
+Q2_agent.learning=False
+
+
+# In[17]:
+
+
+g=Game(number_of_games=1000)
+g.display=False
+result=g.run(minimax_agent,Q2_agent)
+g.report()
+
+
+# In[18]:
+
+
+g=Game(number_of_games=1000)
+g.display=False
+result=g.run(random_agent,minimax_agent)
+g.report()
+
+
+# In[19]:
+
+
+g=Game(number_of_games=1000)
+g.display=False
+result=g.run(random_agent,Q2_agent)
+g.report()
+
+
+# In[ ]:
+
+
+
+
